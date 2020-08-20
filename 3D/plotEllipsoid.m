fc2f5a56d577bd874768b8af4b1872d0175e76e7
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2020-08-20 14:10:25 +0545

    Refactor and comment

--- 3D/plotEllipsoid.m
+++ 3D/plotEllipsoid.m
@@ -6,66 +6,106 @@ function [] = plotEllipsoid(v)
 %   v = [a,b,c,f,g,h,p,q,r,d], a vector corresponding to coefficients of 
 %       the general quadric surface given by equation,  
 %       ax2 + by2 + cz2 + 2fyz + 2gxz + 2hxy + 2px + 2qy + 2rz + d = 0.
-% 
+%
+% Notes:
+% 1.General ellipsoid in matrix form: X'*M*X + [2p,2q,2r]*X + d = 0
+%   where,
+%       X = [x,y,z]
+%       M = [a h g; h b f; g f c]
+%
+% 2.Diagonalizing above matrix M to M_ rotates the general ellipsoid such 
+%   that its axes aligns with new coordinate axes defined by eigenvectors.
+%   New ellipsoid: X'*C'*M*C*X + [2p,2q,2r]*C*X + d = 0
+%   where,    
+%       C  = Eigenvector matrix of M (DCM from old to new coordinates)
+%       M_ = C'*M*C= New ellipsoid diagonal matrix
+%
+%
+% 3.Semi principal axes (ax,by,cz) for ellipsoid with no rotation
+%       ax = sqrt(p^2/a^2 + q^2/(a*b) + r^2/(a*c) - d/a);
+%       by = sqrt(p^2/(a*b) + q^2/b^2 + r^2/(b*c) - d/b);
+%       cz = sqrt(p^2/(a*c) + q^2/(b*c) + r^2/c^2 - d/c);
+%   here, f = 0, g = 0 and h = 0
+%
+% Steps:
+%   1) Diagonalize M matrix using eigenvector of M (M_ = C'*M*C)
+%   2) Find p,q,r of new matrix ([p_,q_,r_] = [p,q,r]*C)
+%   3) Find semi principal axes (ax_,ay_,az_) for f,g and h = 0
+%   4) Generate points of ellipsoid using parametric equation
+%   5) Rotate the points using DCM
+%   6) Plot the points  
+%
 % Rishav (2020/6/8)
 
 % Unpack ellipsoid coefficients
-a = v(1);
-b = v(2);
-c = v(3);
-f = v(4); 
-g = v(5); 
-h = v(6); 
-p = v(7);
-q = v(8);
-r = v(9); 
+a = v(1); b = v(2); c = v(3);
+f = v(4); g = v(5); h = v(6); 
+p = v(7); q = v(8); r = v(9); 
 d = v(10); 
 
-M =[a h g; h b f; g f c];
-% Q = [a h g p; h b f q; g f c r; p q r d];
+%%% Coordinate frame transformation i.e diagonalize M 
+M =[a h g; h b f; g f c]; % Original ellipsoid matrix 
+[evec,~]=eig(M); % Compute eigenvectors matrix
+C = evec; % DCM = eigenvectors matrix 
+M_ = C'*M*C; % Diagonalize M
 
-[evec,eval]=eig(M)
-disp(det(-evec));
-C = evec;
-M_ = C'*M*C;
+% Coefficients of the ellipsoid in new frame
+% Note the ellipsoid is not rotating in this new frame so f, g and h = 0 
+pqr_ = [p,q,r]*C;   
 a_ = M_(1,1);
 b_ = M_(2,2);
 c_ = M_(3,3);
-pqr_ = [p,q,r]*C;   
 p_ = pqr_(1);
 q_ = pqr_(2);
 r_ = pqr_(3);
 d_ = d;
 
+% Semi principal axes (Still no rotation)
 ax_ = sqrt(p_^2/a_^2 + q_^2/(a_*b_) + r_^2/(a_*c_) - d_/a_);
 bx_ = sqrt(p_^2/(a_*b_) + q_^2/b_^2 + r_^2/(b_*c_) - d_/b_);
 cx_ = sqrt(p_^2/(a_*c_) + q_^2/(b_*c_) + r_^2/c_^2 - d_/c_);
 
+% Centre of the ellipsoid
+centre = M\[-p, -q, -r]';
+
+% Generate ellipsoid points
 theta = linspace(0,2*pi,20);
 phi = linspace(0,pi,20);
 [theta,phi] = meshgrid(theta,phi);
-
 x_ = ax_*cos(theta).*cos(phi);
 y_ = bx_*cos(theta).*sin(phi);
 z_ = cx_*sin(theta);
 
+%%% Note
+% Till now we have done and extracted all the information out from the
+% input coefficients of ellipsoid. We generated the points too but it must
+% be remembered that these points are described in the coordinate
+% represented by eigenvectors where there is no rotation of ellipsoid. Now
+% all that remains is to rotate each points using DCM (C) that we found
+% above.
+
+%%% Uncomment this to view unrotated ellipsoid
 % mesh(x_,y_,z_,'FaceAlpha','0.5')
 % axis equal
-hold on;
+% hold on;
 
-x_ = x_(:);
-y_ = y_(:);
-z_ = z_(:);
-
-% Centre of the ellipsoid
-centre = M\[-p, -q, -r]';
-
+% Rotate ellipsoid 
+grid_size = length(x_);
+x_ = x_(:); y_ = y_(:); z_ = z_(:);
 for i_iters = 1:length(x_)
     after_rot = C*[x_(i_iters), y_(i_iters), z_(i_iters)]';
     x_(i_iters) = centre(1) + after_rot(1);
     y_(i_iters) = centre(2) + after_rot(2);
     z_(i_iters) = centre(3) + after_rot(3);
-end  
+end
 
-plot3(x_,y_,z_); % Input points
+% Vector to mesh
+x_ = reshape(x_,grid_size,grid_size);
+y_ = reshape(y_,grid_size,grid_size);
+z_ = reshape(z_,grid_size,grid_size);
+
+%%% Plot the ellipsoid corresponding to input coefficients
+mesh(x_,y_,z_,'FaceAlpha','0.5')
+axis equal
+hold on;
 end
--- 3D/testEllipsoidFit.m
+++ 3D/testEllipsoidFit.m
@@ -4,53 +4,56 @@ clc
 clear 
 close all
 
+%%%%% Input Ellipsoid parameters for test %%%%%
+% Semi principal axes
+ax= 20; 
+bx = 30; 
+cx = 50;  
+%
+% Centre
+xc = 10; 
+yc = 100; 
+zc = 10;  
+centre = [xc,yc,zc];
+%
+% Rotation angles (3-2-1)
+yaw = pi/4; 
+pitch = pi/3; 
+roll =pi/7;  
+%
+% Signal to noise ratio
+% White gaussian noise is added
+SNR = 20;
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %%% Generate test points
 % x	=	a*cosu*sinv 	
 % y	=	b*sinu*sinv	
 % z	=	c*cosv
 % u in [0,2pi) and v in [0,pi]
 [u, v] = meshgrid(0:0.3:pi*2,0:0.3:pi);
+x = ax*cos(u).*cos(v);
+y = bx*cos(u).*sin(v);
+z = cx*sin(u);
 
-% a = 10; 
-% b = 10; 
-% c = 50;
-% xc = 10;
-% yc = 100;
-% zc = 10;
-
-% Rotation angles of the ellipsoid (3-2-1 Euler angles) 
-yaw = pi; 
-pitch = pi; 
-roll =pi/7; % ROll
-
-a = 10;
-b = 20;
-c = 30;
-
-xc = 10;
-yc = 15;
-zc = 10;
-
-% Parametric equations of ellipsoids 
-x = a*cos(u).*cos(v);
-y = b*cos(u).*sin(v);
-z = c*sin(u);
-x=x(:); y=y(:); z=z(:); % Meshgrid to vector
+% Meshgrid to vector
+x=x(:); y=y(:); z=z(:); 
 xyz = [x y z];
 
 % Rotate using DCM (321)
-C = dcm321Euler(yaw,pitch,roll)
+C = dcm321Euler(yaw,pitch,roll);
 for i_iters = 1: length(x)
      new = C*xyz(i_iters,:)';
      xyz(i_iters,:) = new'; 
 end
 
+% Move centre after rotation
 x = xc + xyz(:,1);
 y = yc + xyz(:,2);
 z = zc + xyz(:,3);
 
 % Add noise to generated points
-SNR = 1000;
 x = awgn(x,SNR,'measured');
 y = awgn(y,SNR,'measured');
 z = awgn(z,SNR,'measured');
@@ -58,10 +61,8 @@ z = awgn(z,SNR,'measured');
 %%% Ellipse fit algoritm
 v = ellipsoidFit(x,y,z);
 
-% Plot and camera settings
-% set(gca,'NextPlot','add', 'Visible','off');
-% view(59,13); hold on
-
 %%% Plot
-plotEllipsoid(v); % Estimated ellipse
-plot3(x,y,z,'.'); % Input points
+% Plot and camera settings
+% set(gca,'NextPlot','add', 'Visible','off'); view(59,13); hold on;
+plot3(x,y,z,'.'); hold on; % Input points
+plotEllipsoid(v); % Estimated ellipsoid
